b8cbd9d947eef4e0382942da02d36e44355cfe30
admin <edelvarden@gmail.com>
2023-03-04 09:33:55 +0200

    refactoring

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
-// let cityName = store.get('user').city;
-// let apikey = store.get('user').api;
-
 const cityField = document.querySelector("#city");
 const keyField = document.querySelector("#key");
 const submitButton = document.querySelector("#submit");
 
+const saveLocal = (city, key) => {
+    const isCityValid = city && typeof city === "string" && city.trim().length > 0;
+    const isKeyValid = key && typeof key === "string" && key.trim().length > 0;
 
-function saveLocal(city, key) {
-    city = `${city}`.trim();
-    key = `${key}`.trim();
-    if (city && key) {
-        store.set('user', { city: city, key: key });
+    if (isCityValid && isKeyValid) {
+        store.set('user', { city: city.trim(), key: key.trim() });
+    } else {
+        console.error("Incorrect values for city or key");
     }
-    else console.log("incorrect values");
 }
 
-submit.addEventListener("click", () => {
-    saveLocal(cityField.value, keyField.value);
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const cityFieldValue = cityField && cityField.value ? cityField.value : '';
+    const keyFieldValue = keyField && keyField.value ? keyField.value : '';
+
+    saveLocal(cityFieldValue, keyFieldValue);
 
     const city = getLocal("city");
     const key = getLocal("key");
@@ -24,171 +27,91 @@ submit.addEventListener("click", () => {
     getWeather(city, key);
 });
 
-function getLocal(value) {
+const getLocal = (value) => {
     let localValue = "";
-    try { localValue = store.get('user')[`${value}`] } catch (error) { console.log(error) }
-    // console.log(localValue);
+    try {
+        localValue = store.get('user')[value];
+    } catch (error) {
+        console.error(error);
+    }
     return localValue;
 }
-
 
 window.addEventListener("load", () => {
     const city = getLocal("city");
     const key = getLocal("key");
 
-    cityField.value = city;
-    keyField.value = key;
+    cityField.value = (city ? city : '');
+    keyField.value = (key ? key : '');
 
     getWeather(city, key);
 });
 
-function k2C(kelvin) {
-    return kelvin - 273.15;
-}
+const k2C = (kelvin) => kelvin - 273.15;
 
 function getWeather(city, key) {
     const panel = document.querySelector("#panel");
 
     if (city && key) {
 
-        // for week
-        const api_url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}`;
+        // define constants using const
+        const API_URL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}`;
 
-        getapi(api_url);
+        // Use arrow function syntax for getapi
+        const getapi = async (url) => {
+            try {
+                // fetch returns a promise that will reject if the response isn't ok
+                const response = await fetch(url);
 
-        // Defining async function
-        async function getapi(url) {
-
-            // Storing response
-            const response = await fetch(url);
-
-            // Storing data in form of JSON
-            const data = await response.json();
-            console.log(data);
-            showWeek(data);
+                // instead of storing JSON as a variable, return it directly from the function
+                return response.json();
+            } catch (error) {
+                console.error(error);
+                throw new Error(error.message);
+            }
         }
 
-        function showWeek(data) {
-            data.list.map(item => {
-                show(item);
-            });
-        }
+        // Use arrow function syntax for showWeek
+        const showWeek = (data) => {
+            data.list.map(item => show(item));
+        };
 
-        function show(data) {
+        // call getapi and use then() to handle the returned data
+        getapi(API_URL)
+            .then(data => showWeek(data));
 
-            let id = data.weather[0].id; // Clouds
-            let description = data.weather[0].description; // overcast clouds
+        // Use arrow function instead of named function
+        const show = (data) => {
+            // Destructure nested data object to get id and description values
+            const { id } = data.weather[0];
+            const { description } = data.weather[0];
 
-            function getIcon(id) {
-                let iconId = "";
+            // Define icons based on their corresponding ids in an object rather than multiple arrays
+            const icons = {
+                '01d': [800],
+                '02d': [801],
+                '03d': [802],
+                '04d': [803, 804],
+                '09d': [300, 301, 302, 310, 311, 312, 313, 314, 321, 520, 521, 522, 531],
+                '10d': [500, 501, 502, 503, 504],
+                '11d': [200, 201, 202, 210, 211, 212, 221, 230, 231, 232],
+                '13d': [511, 600, 601, 602, 611, 612, 613, 615, 616, 620, 621, 622],
+                '50d': [701, 711, 721, 731, 741, 751, 761, 762, 771, 781],
+            };
 
-                // Group 2xx: Thunderstorm
-                let clear = [
-                    800
-                ];
-                let fewClouds = [
-                    801
-                ];
-                let scatteredClouds = [
-                    802
-                ];
-                let brokenClouds = [
-                    803,
-                    804
-                ];
-                let showerRain = [
-                    300,
-                    301,
-                    302,
-                    310,
-                    311,
-                    312,
-                    313,
-                    314,
-                    321,
-                    520,
-                    521,
-                    522,
-                    531,
-                ];
+            // Define a function to find matching icon based on passed id
+            const getIcon = (id) => {
+                let iconId = '';
 
-                let rain = [
-                    500,
-                    501,
-                    502,
-                    503,
-                    504,
-                ];
-                let thunderstorm = [
-                    200,
-                    201,
-                    202,
-                    210,
-                    211,
-                    212,
-                    221,
-                    230,
-                    231,
-                    232,
-                ];
-                let snow = [
-                    511,
-                    600,
-                    601,
-                    602,
-                    611,
-                    612,
-                    613,
-                    615,
-                    616,
-                    620,
-                    621,
-                    622,
-                ];
-                let mist = [
-                    701,
-                    711,
-                    721,
-                    731,
-                    741,
-                    751,
-                    761,
-                    762,
-                    771,
-                    781,
-                ];
-
-
-                if (clear.indexOf(id) !== -1) {
-                    iconId = "01d";
-                }
-                if (fewClouds.indexOf(id) !== -1) {
-                    iconId = "02d";
-                }
-                if (scatteredClouds.indexOf(id) !== -1) {
-                    iconId = "03d";
-                }
-                if (brokenClouds.indexOf(id) !== -1) {
-                    iconId = "04d";
-                }
-                if (showerRain.indexOf(id) !== -1) {
-                    iconId = "09d";
-                }
-                if (rain.indexOf(id) !== -1) {
-                    iconId = "10d";
-                }
-                if (thunderstorm.indexOf(id) !== -1) {
-                    iconId = "11d";
-                }
-                if (snow.indexOf(id) !== -1) {
-                    iconId = "13d";
-                }
-                if (mist.indexOf(id) !== -1) {
-                    iconId = "50d";
+                for (let key in icons) {
+                    if (icons[key].indexOf(id) !== -1) {
+                        iconId = key;
+                        break;
+                    }
                 }
 
                 return `${iconId}.png`;
-            }
+            };
 
             let icon = getIcon(id);
             let iconComponent = `<img src="./icons/${icon}" alt="${description}">`
